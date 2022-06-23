d526e51f1cc10fe48ea1c738f986099114204b50
Asa-KingG <105037519+Asa-KingG@users.noreply.github.com>
2022-06-23 22:16:54 +0100

    Add files via upload

---
+++ life.js
@@ -0,0 +1,7 @@
+var userName = "My name is Ejeh Onyinyechukwu Chinwendu.";
+var userHeight = "My height is 4 ft.";
+var userCountry = "I reside in Nigeria.";
+
+console.log(userName);
+console.log(userHeight);
+console.log(userCountry);
